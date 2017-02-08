d0141db24524df356acd231f6482c7a1d92ba7be
Hošek Jakub <JHosek@csas.cz>
2017-02-08 13:27:59 +0100

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+function getRandomFace() {
+    var faces = [
+        'O-O',
+        'D?D',
+        '<ˇ_ˇ>',
+        '((≡^⚲͜^≡))'
+    ];
+
+    return faces[0];
+}
+
+exports.getRandomFace = getRandomFace;
